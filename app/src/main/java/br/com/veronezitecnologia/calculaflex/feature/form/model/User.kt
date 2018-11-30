0ed316208880ac31f420ff755e30ea28eecebc13
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-29 22:59:16 -0200

    adição do firebase

---
+++ app/src/main/java/br/com/veronezitecnologia/calculaflex/feature/form/model/User.kt
@@ -0,0 +1,7 @@
+package br.com.veronezitecnologia.calculaflex.feature.form.model
+
+data class User(
+        val nome: String = "",
+        val email: String = "",
+        val fone: String = ""
+)
